cd68932234b5809db5305575b501ba6cecb2725b
Gerardo González <wagegosa@gmail.com>
2020-06-01 13:04:25 -0500

    Se crea actualiza función donde al car un click en la matriz se muestra una X.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 //Boton
 class Square extends React.Component {
-    render() {
-      return (
-        <button className="square" onClick={function(){console.log
-        ('clic');}}>
-          {this.props.value}
+  constructor(props){ 
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+  render() {
+    return (
+      <button className="square" onClick= { ()=> this.setState({value: 'X'})}>
+        {this.state.value}
         </button>
-      );
+        );
     }
   }
   //Matriz
@@ -17,53 +22,51 @@ class Square extends React.Component {
     renderSquare(i) {
       return <Square value={i} />;
     }
-  
     render() {
       const status = 'Next player: X';
-  
       return (
         <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
         </div>
-      );
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+        </div>
+        );
+      }
     }
-  }
-  //Juega
-  class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
+    //Juega
+    class Game extends React.Component {
+      render() {
+        return (
+          <div className="game">
+            <div className="game-board">
             <Board />
           </div>
           <div className="game-info">
             <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
+              <ol>{/* TODO */}</ol>
+            </div>
           </div>
-        </div>
-      );
+        );
+      }
     }
-  }
-  
-  // ========================================
-  
-  ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-  );
-  
+    
+    // ========================================
+    
+    ReactDOM.render(
+      <Game />,
+      document.getElementById('root')
+    );
+    
