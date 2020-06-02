ad7e7145eedcdbc90f1eef885f254062e11d08e1
Gerardo González <wagegosa@gmail.com>
2020-06-02 06:52:42 -0500

    Se crea modific archiva para cuando demos click se indique X o O.

--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //Boton
-class Square extends React.Component {
-    render() {
-        return (
-            <button
-            className="square"
-            onClick={() => this.props.onClick()}
-            >
-            {this.props.value}
-            </button>
-        );
-    }
+function Square(props) {
+    return(
+        <button
+            className= "square"
+            onClick= {props.onClick}
+        >
+            {props.value}
+        </button>
+    );
 }
 //Matriz
 class Board extends React.Component {
@@ -20,12 +18,16 @@ class Board extends React.Component {
       super(props);
       this.state = {
         squares: Array(9).fill(null),
+        xIsNext: true,
       };
     }
     handleClick(i) {
       const squares = this.state.squares.slice();
-      squares[i] = 'X';
-      this.setState({squares: squares});
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
     }
     renderSquare(i) {
       return (
@@ -36,7 +38,7 @@ class Board extends React.Component {
       );
     }
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'x' : 'O');
         return (
             <div>
                 <div className="status">{status}</div>
