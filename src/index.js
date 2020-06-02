adc0c42d9662e84d6f67042cbc4bea0a69991d47
Gerardo González <wagegosa@gmail.com>
2020-06-02 05:34:58 -0500

    Se eleimina el contructpr de squsre y se medifica el botón, se agrega el constructor de square, se cre haleclick y se modifica el retunr de renderSquare.

--- src/index.js
+++ src/index.js
@@ -3,70 +3,83 @@ import ReactDOM from 'react-dom';
 import './index.css';
 //Boton
 class Square extends React.Component {
-  constructor(props){ 
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-  render() {
-    return (
-      <button className="square" onClick= { ()=> this.setState({value: 'X'})}>
-        {this.state.value}
-        </button>
+    render() {
+        return (
+            <button
+            className="square"
+            onClick={() => this.props.onClick()}
+            >
+            {this.props.value}
+            </button>
         );
     }
-  }
-  //Matriz
-  class Board extends React.Component {
+}
+//Matriz
+class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      };
+    }
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
+    }
     renderSquare(i) {
-      return <Square value={i} />;
+      return (
+        <Square
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
+        />
+      );
     }
     render() {
-      const status = 'Next player: X';
-      return (
-        <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-        </div>
-        );
-      }
-    }
-    //Juega
-    class Game extends React.Component {
-      render() {
+        const status = 'Next player: X';
         return (
-          <div className="game">
-            <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-              <ol>{/* TODO */}</ol>
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
             </div>
-          </div>
         );
-      }
     }
+}
+//Juega
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+            <div className="game-board">
+                <Board />
+            </div>
+            <div className="game-info">
+                <div>{/* status */}</div>
+                <ol>{/* TODO */}</ol>
+            </div>
+            </div>
+        );
+    }
+}
     
-    // ========================================
-    
-    ReactDOM.render(
-      <Game />,
-      document.getElementById('root')
-    );
+// ========================================
+      
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
     
